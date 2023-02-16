51bf3bc0f254e7aaff6b4c7e5ee595e9e2c296d4
Jeric <marianovjeric@gmail.com>
2023-02-16 11:11:02 -0800

    created Todo component

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <div>
+        <h2>Finish Frontend Simplified</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Finish the interview section</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Land a job</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
