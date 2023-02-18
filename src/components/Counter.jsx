62b3007e29a312832e39a6d61443764b8d066c92
Jeric <marianovjeric@gmail.com>
2023-02-18 00:10:34 -0800

    using objects in react with the counter example and useState

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    const [counter, setCounter] = useState(0)
+    const [cart, setCart] = useState({ 
+        item: 'apple', 
+        quantity: 0,
+    })
 
-    function incrementCounter() {
-        setCounter((prevCounter) => prevCounter + 1)
-        setCounter((prevCounter) => prevCounter + 1)
+    function removeApple() {
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity - 1
+        }))
     }
 
-    function decrementCounter() {
-        setCounter((prevCounter) => prevCounter - 1)
-        setCounter((prevCounter) => prevCounter - 1)
+    function addApple() {
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity + 1,
+        }))
     }
-
 
     return (
         <>
-        <button onClick={decrementCounter}>-</button>
-        {counter}
-        <button onClick={incrementCounter}>+</button>
+        <button onClick={removeApple}>-</button>
+        {cart.quantity}
+        {cart.item}
+        <button onClick={addApple}>+</button>
         </>
     )
 }
