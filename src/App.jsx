4a3338f9bd739e752cb52f1177b3b7154d585a9d
Jeric <marianovjeric@gmail.com>
2023-02-17 23:31:26 -0800

    first useState example, importing useState and using it

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import React, { useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false) 
+
+
   return (
     <div>
       <Title />
       <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button>Add todo</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
         <Todo title="Finish Frontend Simplified" />
         <Todo title="Finish Interview Section" />
         <Todo title="Land a job" />
       </div>
-      <Modal 
-      title="Are you sure you want to delete?"
-      />
+      {showModal && <Modal title="Confirm Delete?" />}
     </div>
   );
 }
